7d667014d5224bd326b07ed02f8766369ca81c2e
huda <huda23aduh@gmail.com>
2022-04-07 14:50:44 +0700

    first commit

---
+++ src/Pkg2Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Avanahuda\Pkg2;
+
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+
+class Pkg2Controller extends Controller
+{
+    public function index($timezone)
+    {
+        echo "abbb";
+    }
+}
---
+++ src/Pkg2ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Avanahuda\Pkg2;
+
+use Illuminate\Support\ServiceProvider;
+
+class Pkg2ServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
